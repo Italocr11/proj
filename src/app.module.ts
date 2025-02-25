2de9e100371abb3ccc028588a2b803a834d62609
Italo Rael <101028774+Italocr11@users.noreply.github.com>
2025-02-25 11:41:17 -0300

    Recuperar senha

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ReservaModule } from './modules/reservas.module';
+import { UsuariosModule } from './modules/usuarios.module';
+import { NotificacaoModule } from './modules/notif.module';
 
 @Module({
-  imports: [],
+  imports: [
+        TypeOrmModule.forRoot({
+          type: 'sqlite',
+          database: 'database.sqlite',
+          entities: [__dirname + '/**/*.entity{.ts,.js}'],
+          synchronize: true, // Apenas para desenvolvimento
+        }),
+        ReservaModule,
+        UsuariosModule,
+        NotificacaoModule,
+      ],
   controllers: [AppController],
   providers: [AppService],
 })
